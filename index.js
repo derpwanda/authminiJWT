723556933a8d8bf5e7c53121dd65c58ea2f17a4c
Wanda <doubleuevans@gmail.com>
2018-12-05 14:12:26 -0600

    applied JWT; functional

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const db = require('./database/dbConfig.js');
 
@@ -8,6 +10,44 @@ const server = express();
 
 server.use(express.json());
 server.use(cors());
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    roles: ['sales', 'marketing'], //added manually here; normally would come from db
+  }
+
+  //const secret = 'afoiu2389u_caiocja;l3?vu80vnqa909jk&claksma';
+  const secret = process.env.JWT_SECRET;
+
+  const options = {
+    expiresIn: '1h',
+  }
+  return jwt.sign(payload, secret, options)
+}
+
+function protected(req, res, next) {
+  //token manually sent in the Authorization header
+  const token = req.headers.authorization;
+
+  if (token) {
+    //is valid
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if(err) {
+        //token is invalid
+        res.status(401).json({ message: 'token invalid' })
+      } else {
+        //token is valid
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    //bounced, is invalid
+    res.status(401).json({ message: 'no token provided (protected)'})
+  }
+}
 
 //endpoint for login
 server.post('/api/login', (req,res) => {
@@ -20,13 +60,16 @@ server.post('/api/login', (req,res) => {
     .then(user => {
       if(user && bcrypt.compareSync(creds.password, user.password)) { //see COMPARESYNC
         //passwords match and user exists by that username
-        res.status(200).json({message: 'you made it!'})
+        //create a token
+        //send token manually
+        const token = generateToken(user);
+        res.status(200).json({message: 'you made it!', token})
       } else {
         //either username or password is valid, all returns failure
         res.status(401).json({message: 'incorrect inputs'})
       }
     })
-    .catch(err => res.json(err))
+    .catch(err => res.json({message: 'no'}))
 })
 
 //create endpoint for registration
@@ -52,16 +95,6 @@ server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
 
-function protected(req, res, next) {
-/*   // restricts access to only authenticated users
-  if (req.session && req.session.userId) {
-    next();
-  } else {
-    //bounce them
-    res.status(401).json({ message: 'not allowed'})
-  } */
-  next();
-}
 
 // protect this route, only authenticated users should see it
 server.get('/api/me', protected, (req, res) => {
---
+++ notes.md
@@ -0,0 +1,36 @@
+
+install yarn
+install express cors bcrypt
+install dev nodemon
+install yarn add jsonwebtoken
+
+add const jwt = require('jsonwebtoken') to require list
+add token generator function - generateToken()
+  -pass object generateToken(OBJECT)
+    -use const to create payload
+    -use const to create secret //weird collection of characters
+    -use const to create options //eg., token expiration, 
+  -return jwt.sign(payload, secret, options)
+add generator function to ...
+  -return token object (if you need to. way to see if it was created)
+
+#create a protected enpoint (aka middleware)
+created a protected function
+  create a const to references the token
+  if the token is valid move ahead
+    -use library function .verify()
+    -pass token AND jwt secret 
+  else return error/bounce
+
+#to hold token secret in env
+yarn add dotenv
+create .env file in root of project
+  name variable and set equal to secret string (quotation marks dont matter)
+in index.js add:
+  -require('dotenv).config(); //to very top of index; yarn add dotenv
+  -in generateToken function, set secret variable to `process.end.VARIABLE_NAME`
+now you can reference secret by `process.end.VARIABLE_NAME` when need (ie, protected rounts)
+
+#put token inside of cookie
+-add req.session.jwt (jwt is based on name of jasonwebtoken used see line 5) and set it to the generated token variable inside route/midware
+-because you have set the token to req.session, in protected (if you are using it) you now need to change the token from req.headers.authorization to req.session.jwt
