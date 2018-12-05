5d0645a0b0f900f7af33805ed43a31deec770ed3
Wanda <doubleuevans@gmail.com>
2018-12-05 11:20:57 -0600

    JWT not added; sessions removed; functional

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const session = require('express-session');
-const KnexSessionStore = require('connect-session-knex')(session);
 const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 
 const server = express();
-//create config object
-const sessionConfig = {
-  name: 'lisa', // default is connect.sid
-  secret: 'asdf;lkj',
-  cookie: {
-    maxAge: 1000 * 60 * 10,
-    secure: false, // only set it over https; in production you want this true
-  },
-  httpOnly: true, //no js can change/touch this code
-  resave: false,
-  saveUnitialized: false,
-  //added after installing connect-session-knex
-  //stores session
-  store: new KnexSessionStore({
-    tablename: 'sessions',
-    sidfieldname: 'sid', //session id field name
-    knex: db,
-    createtable: true,
-    clearInterval: 1000 * 60 * 60,
-  })
-}
 
-server.use(session(sessionConfig)); //wireup Sessionconfig management
 server.use(express.json());
 server.use(cors());
 
@@ -43,8 +19,6 @@ server.post('/api/login', (req,res) => {
     .first()
     .then(user => {
       if(user && bcrypt.compareSync(creds.password, user.password)) { //see COMPARESYNC
-        //set user id to be used in permissions
-        req.session.userId = user.id;
         //passwords match and user exists by that username
         res.status(200).json({message: 'you made it!'})
       } else {
@@ -79,13 +53,14 @@ server.get('/', (req, res) => {
 });
 
 function protected(req, res, next) {
-  // restricts access to only authenticated users
+/*   // restricts access to only authenticated users
   if (req.session && req.session.userId) {
     next();
   } else {
     //bounce them
     res.status(401).json({ message: 'not allowed'})
-  }
+  } */
+  next();
 }
 
 // protect this route, only authenticated users should see it
@@ -110,19 +85,5 @@ server.get('/api/users', protected, (req, res) => {
       })
       .catch(err => res.send(err));
 });
-
-server.get('/api/logout', (req, res) => {
-  if(req.session) {
-    req.session.destroy(err => {
-      if (err) {
-        res.send('no logout')
-      } else {
-        res.send('goodbye')
-      }
-    })
-  } else {
-    res.end()
-  }
-})
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
